c0b8574dafbdefc2c48118d6d1ccf626cdf13b30
Connor Spangler <connormspangler@gmail.com>
2019-11-24 22:28:52 -0800

    Additional zoom logic

--- dev/jide.py
+++ dev/jide.py
@@ -1,14 +1,14 @@
 from PyQt5 import QtWidgets, QtGui, uic
 from PyQt5.QtGui import QPalette, QColor, QIcon, QPen, QBrush
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtCore import Qt, QSize, QEvent
 import sys
 import os
 
 class GraphicsScene(QGraphicsScene):
     def __init__(self, parent=None):
         QGraphicsScene.__init__(self, parent)
-        self.setSceneRect(-100, -100, 200, 200)
+        self.setSceneRect(0, 0, 1000, 1000)
         self.opt = ""
 
     def setOption(self, opt):
@@ -20,7 +20,7 @@ class GraphicsScene(QGraphicsScene):
         x = event.scenePos().x()
         y = event.scenePos().y()
         if self.opt == "Generate":
-            self.addEllipse(x, y, 4, 4, pen, brush)
+            self.addRect(x, y, 4, 4, pen, brush)
         elif self.opt == "Select":
             print(x, y)
 
@@ -33,8 +33,8 @@ class jide(QtWidgets.QMainWindow):
 
         self.scene = GraphicsScene(self)
         self.graphicsView.setScene(self.scene)
+        self.scene.installEventFilter(self)
         self.graphicsView.setAlignment(Qt.AlignLeft | Qt.AlignTop)
-
 
         group = QButtonGroup(self)
         group.addButton(self.radioButton)
@@ -44,10 +44,28 @@ class jide(QtWidgets.QMainWindow):
         self.radioButton.setChecked(True)
         self.scene.setOption(self.radioButton.text())
 
-    def wheelEvent(self, event):
-        if event.angleDelta().y() < 0:
-            print("Zooming out: {0}".format(event.angleDelta().y()))
-            self.graphicsView.scale(1/1.2, 1/1.2)
+    def eventFilter(self, source, event):
+        if event.type() == QEvent.GraphicsSceneWheel and QtWidgets.QApplication.keyboardModifiers() == Qt.ControlModifier:
+            self.zoomCanvas(event)
+            event.accept()
+            return True
+        return False
+
+    def zoomCanvas(self, event):
+        print("{0}, {1}".format(event.scenePos().x(), event.scenePos().y()))
+        zoomInFactor = 1.25
+        zoomOutFactor = 1 / zoomInFactor
+        # Save the scene pos
+        oldPos = event.scenePos()
+        # Zoom
+        if event.delta() > 0:
+            zoomFactor = zoomInFactor
         else:
-            print("Zooming in: {0}".format(event.angleDelta().y()))
-            self.graphicsView.scale(1.2, 1.2)
+            zoomFactor = zoomOutFactor
+        self.graphicsView.scale(zoomFactor, zoomFactor)
+        # Get the new position
+        newPos = event.scenePos()
+        # Move scene to old position
+        delta = newPos - oldPos
+        self.graphicsView.translate(delta.x(), delta.y())
+
