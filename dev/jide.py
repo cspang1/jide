d0c0cf76448af12aed379404ce75adae66cceb2e
Connor Spangler <connormspangler@gmail.com>
2019-11-26 23:26:05 -0800

    Re-integrated ui

--- dev/jide.py
+++ dev/jide.py
@@ -1,9 +1,13 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, uic
 from canvas import GraphicsView
+import sys
+import os
 
 class jide(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        ui_path = os.path.dirname(os.path.abspath(__file__))
+        uic.loadUi(os.path.join(ui_path, "mainwindow.ui"), self)
         self.setWindowTitle("JIDE")
-        self.setCentralWidget(GraphicsView(self))
+        self.setCentralWidget(self.canvas)
         self.setStyleSheet("background-color: #494949;")
