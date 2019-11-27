b21f650e9c886aaebd110d5c2010c3ed29cfef04
Connor Spangler <connormspangler@gmail.com>
2019-11-27 00:24:55 -0800

    Implemented color palette dock

--- dev/colorpalette.py
+++ dev/colorpalette.py
@@ -1,5 +1,29 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
-class ColorPalette(QObject):
-    test = 0
+class ColorPalette(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.setMaximumSize(320, 320)
+        grid = QGridLayout()
+        grid.setHorizontalSpacing(0)
+        grid.setVerticalSpacing(0)
+        self.setLayout(grid)
+
+        positions = [(row,col) for row in range(16) for col in range(16)]
+        colors = [(red, green, blue) for red in range(8) for green in range(8) for blue in range(4)]
+
+        for position, color in zip(positions,colors):
+            label = QLabel()
+            pixmap = QPixmap(20,20)
+            pixmap.fill(QColor(*self.upsample(*color)))
+            label.setPixmap(pixmap)
+            grid.addWidget(label, *position)
+
+    def upsample(self, red, green, blue):
+        red = round((red/7)*255)
+        green = round((green/7)*255)
+        blue = round((blue/3)*255)
+        return (red, green, blue)
--- dev/jide.py
+++ dev/jide.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets, uic
 from canvas import GraphicsView
+from colorpalette import ColorPalette
 import sys
 import os
 
@@ -9,5 +10,11 @@ class jide(QtWidgets.QMainWindow):
         ui_path = os.path.dirname(os.path.abspath(__file__))
         uic.loadUi(os.path.join(ui_path, "mainwindow.ui"), self)
         self.setWindowTitle("JIDE")
-        self.setCentralWidget(self.canvas)
         self.setStyleSheet("background-color: #494949;")
+
+        # Setup canvas
+        self.setCentralWidget(self.canvas)
+
+        # Setup color palette
+        color_palette = ColorPalette()
+        self.colorPalette.setWidget(color_palette)
