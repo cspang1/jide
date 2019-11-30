292af3cf459eb1fffacc7b39030c4420eddbd07f
Connor Spangler <connormspangler@gmail.com>
2019-11-29 22:59:06 -0800

    Added file open

--- dev/jide.py
+++ dev/jide.py
@@ -2,6 +2,7 @@ from PyQt5 import QtWidgets, uic
 from canvas import *
 from PyQt5 import QtCore
 from colorpalette import *
+import json
 import sys
 import os
 
@@ -11,12 +12,12 @@ class jide(QtWidgets.QMainWindow):
 
         # Setup main window
         self.setWindowTitle("JIDE")
-        self.setStyleSheet("background-color: #494949;")
 
         # Setup canvas
         self.scene = GraphicsScene(self)
         self.view = GraphicsView(self.scene, self)
         self.setCentralWidget(self.view)
+        self.view.setStyleSheet("background-color: #494949;")
 
         # Setup color palette dock
         self.colorPaletteDock = QDockWidget("Color Palettes", self)
@@ -24,7 +25,35 @@ class jide(QtWidgets.QMainWindow):
         self.colorPaletteDock.setWidget(self.color_palette)
         self.colorPaletteDock.setFloating(False)
         self.addDockWidget(Qt.RightDockWidgetArea, self.colorPaletteDock)
+        self.color_palette.setStyleSheet("background-color: #494949;")
 
         # Setup color palette signals
         for swatch in self.color_palette.swatches:
             swatch.clicked.connect(self.scene.changeColor)
+
+        # Setup menu bar
+        exitAct = QAction('&Exit', self)
+        exitAct.setShortcut('Ctrl+Q')
+        exitAct.setStatusTip('Exit application')
+        exitAct.triggered.connect(qApp.quit)
+
+        openFile = QAction('&Open', self)
+        openFile.setShortcut('Ctrl+O')
+        openFile.setStatusTip('Open file')
+        openFile.triggered.connect(self.openFile)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAct)
+        fileMenu.addAction(openFile)
+
+    def openFile(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open file", "", "JCAP Resource File (*.jrf)")
+
+        if file_name:
+            with open(file_name, 'r') as f:
+                self.data = json.load(f)
+                for sprite in self.data["sprites"]:
+                    print(sprite["name"])
