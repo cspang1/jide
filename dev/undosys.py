c625756c4dcec1cdda47139a8bb1bdfb50dd1f5b
Connor Spangler <connormspangler@gmail.com>
2019-12-12 16:53:09 -0800

    Added color palette add/remove/rename functionality

--- dev/undosys.py
+++ dev/undosys.py
@@ -18,6 +18,46 @@ class cmdSetSprCol(QUndoCommand):
     def undo(self):
         self.palette[self.name, self.index] = self.original_color
 
+class cmdSetSprColPalName(QUndoCommand):
+    def __init__(self, palette, cur_name, new_name, description, parent=None):
+        super().__init__(description, parent)
+        self.palette = palette
+        self.cur_name = cur_name
+        self.new_name = new_name
+
+    def redo(self):
+        self.palette.setName(self.cur_name, self.new_name)
+
+    def undo(self):
+        self.palette.setName(self.new_name, self.cur_name)
+
+class cmdAddSprColPal(QUndoCommand):
+    def __init__(self, palette, name, contents, description, parent=None):
+        super().__init__(description, parent)
+        self.palette = palette
+        self.name = name
+        self.contents = contents
+
+    def redo(self):
+        self.palette.addPalette(self.name, self.contents)
+
+    def undo(self):
+        self.palette.remPalette(self.name)
+
+class cmdRemSprColPal(QUndoCommand):
+    def __init__(self, palette, name, description, parent=None):
+        super().__init__(description, parent)
+        self.palette = palette
+        self.name = name
+        self.contents = self.palette[self.name]
+        self.index = list(self.palette.keys()).index(self.name)
+
+    def redo(self):
+        self.palette.remPalette(self.name)
+
+    def undo(self):
+        self.palette.addPalette(self.name, self.contents, self.index)
+
 class cmdSetSprPix(QUndoCommand):
     def __init__(self, palette, name, row, col, value, description, parent=None):
         super().__init__(description, parent)
