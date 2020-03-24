de7f6b4ced07ee8dc7bd99023b1d1c090beb3035
cspang1 <connormspangler@gmail.com>
2020-03-24 02:01:06 -0700

    Began implementing dynamic selection size

--- dev/pixelpalette.py
+++ dev/pixelpalette.py
@@ -90,8 +90,8 @@ class PixelPalette(QFrame):
         self.grid.setAlignment(Qt.AlignTop)
         self.setLayout(self.grid)
         self.enabled = False
-        self.width = 4 # DEMO
-        self.height = 4 # DEMO
+        self.select_width = 1 # DEMO
+        self.select_height = 1 # DEMO
 
     def setup(self, data):
         self.data = data
@@ -109,8 +109,9 @@ class PixelPalette(QFrame):
             row = row + 1 if col == 0 else row
             index += 1
         self.setEnabled(True)
-        self.overlay = Overlay(self)
+        self.overlay = Overlay()
         self.overlay.setDims(math.floor(self.contents.__len__()/16))
+        self.grid.addWidget(self.overlay, 0, 0, -1, -1)
         self.selectTiles(next(iter(self.contents)))
 
     pyqtSlot(str, int, int)
@@ -120,6 +121,11 @@ class PixelPalette(QFrame):
         data = self.data.getSprite(name) if self.source == Sources.SPRITE else self.data.getTile(name)
         self.contents[name].updatePixmap(col, row, data[row][col])
 
+    def changeSelectionSize(self, width, height):
+        self.select_width = width
+        self.select_height = height
+        self.selectTiles(self.selected)
+
     pyqtSlot(str)
     def selectTiles(self, name):
         self.selected = name
@@ -127,12 +133,12 @@ class PixelPalette(QFrame):
         data = list(self.data.getSprites()) if self.source == Sources.SPRITE else list(self.data.getTiles())
         num_rows = math.floor(data.__len__() / 16)
         initial_row = math.floor(index/16)
-        if math.floor((index + self.width - 1) / 16) > initial_row:
-            index = math.floor(index/16) * 16 + 16 - self.width
-        if initial_row + self.height > num_rows:
-            index -= 16 * (initial_row + self.height - num_rows)
-        self.subject_selected.emit(index, self.width, self.height)
-        self.overlay.selectTiles(index, self.width, self.height)
+        if math.floor((index + self.select_width - 1) / 16) > initial_row:
+            index = math.floor(index/16) * 16 + 16 - self.select_width
+        if initial_row + self.select_height > num_rows:
+            index -= 16 * (initial_row + self.select_height - num_rows)
+        self.subject_selected.emit(index, self.select_width, self.select_height)
+        self.overlay.selectTiles(index, self.select_width, self.select_height)
 
     pyqtSlot(str)
     def setColorPalette(self, palette):
@@ -141,24 +147,56 @@ class PixelPalette(QFrame):
         for widget in widgets:
             widget.setColors(self.data.sprite_color_palettes[palette])
 
+class Contents(QWidget):
+    def __init__(self, source, palette, parent=None):
+        super().__init__(parent)
+        self.palette = palette
+        control_layout = QHBoxLayout()
+        w_label = QLabel("Width:")
+        self.width = QSpinBox()
+        self.width.setMaximum(16)
+        self.width.setMinimum(1)
+        h_label = QLabel("Height:")
+        self.height = QSpinBox()
+        self.height.setMaximum(8) # HARDCODED FOR DEMO
+        self.height.setMinimum(1)
+        self.width.valueChanged.connect(self.widthChanged)
+        self.height.valueChanged.connect(self.heightChanged)
+        control_layout.addWidget(w_label)
+        control_layout.addWidget(self.width)
+        control_layout.addWidget(h_label)
+        control_layout.addWidget(self.height)
+        scroll_area = QScrollArea(self)
+        self.setFixedWidth(scroll_area.verticalScrollBar().sizeHint().width() + 420) # nice
+        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        scroll_area.setWidget(self.palette)
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(control_layout)
+        main_layout.addWidget(scroll_area)
+        self.setLayout(main_layout)
+
+    @pyqtSlot(int)
+    def widthChanged(self, width):
+        self.palette.changeSelectionSize(width, self.height.value())
+
+    @pyqtSlot(int)
+    def heightChanged(self, height):
+        self.palette.changeSelectionSize(self.width.value(), height)
+
 class PixelPaletteDock(QDockWidget):
     palette_updated = pyqtSignal(str)
 
     def __init__(self, source, parent=None):
         title = "Sprite " if source == Sources.SPRITE else "Tile "
         super().__init__(title + "Palettes", parent)
-        self.source = source
         self.setFloating(False)
         self.setFeatures(QDockWidget.DockWidgetFloatable | QDockWidget.DockWidgetMovable)
-        self.scroll_area = QScrollArea(self)
-        self.scroll_area.setFixedWidth(self.scroll_area.verticalScrollBar().sizeHint().width() + 403)
-        self.scroll_area.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
-        self.scroll_area.setWidgetResizable(True)
-        self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         self.pixel_palette = PixelPalette(source)
+        self.contents = Contents(source, self.pixel_palette)
         self.palette_updated.connect(self.pixel_palette.setColorPalette)
-        self.scroll_area.setWidget(self.pixel_palette)
-        self.setWidget(self.scroll_area)
+        self.setWidget(self.contents)
 
     def setup(self, data):
         self.pixel_palette.setup(data)
