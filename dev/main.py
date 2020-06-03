89a648985878151f9ac6512e2c253aab371a9187
Connor Spangler <connormspangler@gmail.com>
2020-06-02 20:17:35 -0700

    Updated main and requirements for Sphinx autodoc

--- dev/main.py
+++ dev/main.py
@@ -2,11 +2,12 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from jide import jide
 
-app = QApplication(sys.argv)
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
 
-window = jide()
-window.resize(800,600)
-window.showMaximized()
+    window = jide()
+    window.resize(800,600)
+    window.showMaximized()
 
-# Start the event loop.
-app.exec_()
+    # Start the event loop.
+    app.exec_()
