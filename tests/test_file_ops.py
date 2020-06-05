f79ef944c8049d11081cc6c8cdb48eb25d777854
Connor Spangler <connormspangler@gmail.com>
2020-06-05 10:37:02 -0700

    Testing workflow

--- tests/test_file_ops.py
+++ tests/test_file_ops.py
@@ -1,6 +1,10 @@
-import sys
+import sys, os
+
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "src"))
+from jide import jide
 
 
 class TestFileProcs:
-    def test_file_open(self):
-        assert isinstance(0, int)
+    def test_file_open(self, main_window):
+        print("start of test")
+        assert isinstance(main_window, jide)
