c0f07149771cdd68eaf77b13f63c4e9b3d858603
Connor Spangler <connormspangler@gmail.com>
2020-06-05 13:52:56 -0700

    Testing minimal working setup

--- tests/test_file_ops.py
+++ tests/test_file_ops.py
@@ -1,10 +1,19 @@
 import sys, os
 
+# from PyQt5.QtGui import QColor
+
+
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "src"))
 from jide import jide
+from source import Source
 
 
-class TestFileProcs:
-    def test_file_open(self, main_window):
-        print("start of test")
-        assert isinstance(main_window, jide)
+def test_file_open(qtbot):
+    print("start of test")
+    temp = jide()
+    qtbot.addWidget(temp)
+    palettes = temp.data.getColPals(Source.SPRITE)
+    for pal in palettes:
+        for color in pal:
+            print(color.rgb())
+    assert True
