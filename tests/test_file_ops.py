3fb6435befc4069780b2bdabf974b7d36e950ddd
Connor Spangler <connormspangler@gmail.com>
2020-06-04 20:43:39 -0700

    Second attempt

--- tests/test_file_ops.py
+++ tests/test_file_ops.py
@@ -1,7 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication
 
 
 class TestFileProcs:
-    def test_file_open(self, main_window):
+    def test_file_open(self):
         assert isinstance(0, int)
