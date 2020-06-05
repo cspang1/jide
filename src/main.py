5ef128a63237966009209ec681b52e21dc7bfbae
Connor Spangler <connormspangler@gmail.com>
2020-06-04 19:59:45 -0700

    Another definitely pleasant refactor for pytest

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from src.jide import jide
+from jide import jide
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
---
+++ tests/test_silly.py
@@ -0,0 +1,3 @@
+class TestFileProcs:
+    def test_file_open(self, main_window):
+        print(main_window)
