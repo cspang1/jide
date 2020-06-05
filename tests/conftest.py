787ba5e259ccbfe4c77ef4bd23d7bae613263e69
Connor Spangler <connormspangler@gmail.com>
2020-06-05 10:37:02 -0700

    Testing workflow

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import sys, os
+from PyQt5.QtWidgets import QApplication
 
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "src"))
 from jide import jide
@@ -7,6 +8,7 @@ from jide import jide
 
 @pytest.fixture
 def main_window(qtbot):
+    print("start of session")
     widget = jide()
     qtbot.addWidget(widget)
     return widget
