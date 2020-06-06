f0937d1f402f95edb154b02575d91f4a94ef145f
Connor Spangler <connormspangler@gmail.com>
2020-06-06 12:35:28 -0700

    Testing multiline autodocs

--- tests/test_file_ops.py
+++ tests/test_file_ops.py
@@ -1,15 +1,13 @@
 import json
-import sys, os
+import sys
+import os
 from pathlib import Path
-from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QColor
 import pytest
 
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "src"))
-from jide import jide
-from source import Source
-from colorpicker import upsample
+from jide import jide               # noqa: E402
+from colorpicker import upsample    # noqa: E402
 
 dat_path = Path(__file__).parents[1] / "data" / "demo.jrf"
 
