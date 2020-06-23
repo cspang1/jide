70c224852c218f68a0de33a7a2d667fda6fa091f
Connor Spangler <connormspangler@gmail.com>
2020-06-22 17:59:14 -0700

    Tried out some parametrization

--- tests/test_file_ops.py
+++ tests/test_file_ops.py
@@ -8,6 +8,7 @@ import pytest
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "src"))
 from jide import jide  # noqa: E402
 from colorpicker import upsample  # noqa: E402
+from source import Source  # noqa: E402
 
 dat_path = Path(__file__).parents[1] / "data" / "demo.jrf"
 
@@ -30,32 +31,59 @@ class TestDataLoad:
         with open(dat_path, "r") as data_file:
             return json.load(data_file)
 
-    def test_col_pal_names(self, jide_instance, game_data):
-        col_pal_combo_box = jide_instance.sprite_color_palette_dock.color_palette_list
+    @pytest.mark.parametrize(
+        "pal_name, src",
+        [
+            ("spriteColorPalettes", Source.SPRITE),
+            ("tileColorPalettes", Source.TILE),
+        ],
+    )
+    def test_col_pal_names(self, jide_instance, game_data, pal_name, src):
+        tgt = (
+            jide_instance.sprite_color_palette_dock
+            if src is Source.SPRITE
+            else jide_instance.tile_color_palette_dock
+        )
+        col_pal_combo_box = tgt.color_palette_list
         gui_col_pal_names = [
-            col_pal_combo_box.itemText(i) for i in range(col_pal_combo_box.count())
+            col_pal_combo_box.itemText(i)
+            for i in range(col_pal_combo_box.count())
         ]
         dat_col_pals = []
-        for col_pal in game_data["spriteColorPalettes"]:
+        for col_pal in game_data[pal_name]:
             dat_col_pals.append(col_pal["name"])
         assert gui_col_pal_names == dat_col_pals
 
-    def test_col_pal_contents(self, jide_instance, game_data):
-        col_pal_combo_box = jide_instance.sprite_color_palette_dock.color_palette_list
+    @pytest.mark.parametrize(
+        "pal_name, src",
+        [
+            ("spriteColorPalettes", Source.SPRITE),
+            ("tileColorPalettes", Source.TILE),
+        ],
+    )
+    def test_col_pal_contents(self, jide_instance, game_data, pal_name, src):
+        tgt = (
+            jide_instance.sprite_color_palette_dock
+            if src is Source.SPRITE
+            else jide_instance.tile_color_palette_dock
+        )
+        col_pal_combo_box = tgt.color_palette_list
         gui_col_pal_names = [
-            col_pal_combo_box.itemText(i) for i in range(col_pal_combo_box.count())
+            col_pal_combo_box.itemText(i)
+            for i in range(col_pal_combo_box.count())
         ]
-        gui_col_pal = jide_instance.sprite_color_palette_dock.color_palette
-
+        gui_col_pal = tgt.color_palette
         gui_pals = []
         for pal in gui_col_pal_names:
             col_pal_combo_box.setCurrentText(pal)
             gui_pals.append([col.color.rgb() for col in gui_col_pal.palette])
 
-        data_pals = [pal["contents"] for pal in game_data["spriteColorPalettes"]]
+        data_pals = [pal["contents"] for pal in game_data[pal_name]]
         for pal in data_pals:
             pal[:] = [
-                QColor(*upsample(color >> 5, (color >> 2) & 7, color & 3)).rgb()
+                QColor(
+                    *upsample(color >> 5, (color >> 2) & 7, color & 3)
+                ).rgb()
                 for color in pal
             ]
 
